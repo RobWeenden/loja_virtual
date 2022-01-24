1fd461ff79096e4dba8d8e670442dcca24f0d1df
RobWeenden <weendenandrade@gmail.com>
2022-01-24 18:00:56 -0300

    first commit project loja_virt

---
+++ src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/LojaVirtualRwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.rws.lojavirtual.loja_virtual_rws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualRwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualRwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rws/lojavirtual/loja_virtual_rws/LojaVirtualRwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rws.lojavirtual.loja_virtual_rws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualRwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
