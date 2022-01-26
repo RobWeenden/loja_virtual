9ff74b04bf0d689eb673e11f0993261ffaaf5b56
RobWeenden <weendenandrade@gmail.com>
2022-01-25 21:28:16 -0300

    add class endereco for  pessoa

---
+++ src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/Endereco/EnderecoModel.java
@@ -0,0 +1,165 @@
+package br.com.rws.lojavirtual.loja_virtual_rws.Endereco;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import br.com.rws.lojavirtual.loja_virtual_rws.Pessoa.PessoaAbstract;
+
+@Entity
+@Table(name = "tb_endereco")
+@SequenceGenerator(name = "seq_tb_endereco", sequenceName = "seq_tb_endereco", allocationSize = 1, initialValue = 1)
+public class EnderecoModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_tb_endereco")
+    @Column(name = "end_id")
+    private Long id;
+
+    @Column(name = "end_rua")
+    private String rua;
+
+    @Column(name = "end_logradouro")
+    private String logadouro;
+
+    @Column(name = "end_bairro")
+    private String bairro;
+
+    @Column(name = "end_cidade")
+    private String cidade;
+
+    @Column(name = "end_complemento")
+    private String complemento;
+
+    @Column(name = "end_cep")
+    private String cep;
+
+    @Column(name = "end_numero")
+    private String numero;
+
+    @Column(name = "end_uf")
+    private String uf;
+
+    @ManyToOne(targetEntity = PessoaAbstract.class)
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private PessoaAbstract pessoa;
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getLogadouro() {
+        return logadouro;
+    }
+
+    public void setLogadouro(String logadouro) {
+        this.logadouro = logadouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public PessoaAbstract getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(PessoaAbstract pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EnderecoModel other = (EnderecoModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+
+}
