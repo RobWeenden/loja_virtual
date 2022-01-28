d142db0742f70b9386883b5e857475fb8cddeaf3
RobWeenden <weendenandrade@gmail.com>
2022-01-28 12:31:00 -0300

    add class avaliacao_produto

---
+++ src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/AvaliacaoProduto/AvaliacaoProdutoModel.java
@@ -0,0 +1,97 @@
+package br.com.rws.lojavirtual.loja_virtual_rws.AvaliacaoProduto;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import br.com.rws.lojavirtual.loja_virtual_rws.Pessoa.PessoaAbstract;
+import br.com.rws.lojavirtual.loja_virtual_rws.Produto.ProdutoModel;
+
+@Entity
+@Table(name = "tb_avaliacao_produto")
+@SequenceGenerator(name = "seq_avaliacao_produto", sequenceName = "seq_avaliacao_produto", allocationSize = 1, initialValue = 1)
+public class AvaliacaoProdutoModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_avaliacao_produto")
+    @Column(name = "avp_id")
+    private Long id;
+
+    @Column(name = "avp_nota")
+    private Integer nota;
+
+    @ManyToOne(targetEntity = PessoaAbstract.class)
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private PessoaAbstract pessoa;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+    private ProdutoModel produto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public PessoaAbstract getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(PessoaAbstract pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public ProdutoModel getProduto() {
+        return produto;
+    }
+
+    public void setProduto(ProdutoModel produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AvaliacaoProdutoModel other = (AvaliacaoProdutoModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
