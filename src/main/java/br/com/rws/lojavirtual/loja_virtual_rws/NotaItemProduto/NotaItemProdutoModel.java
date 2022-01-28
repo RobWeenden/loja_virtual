66feba605f5f886d7bd39f393b8552d10272b603
RobWeenden <weendenandrade@gmail.com>
2022-01-27 22:33:02 -0300

    add class nota_fical_compra

---
+++ src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/NotaItemProduto/NotaItemProdutoModel.java
@@ -0,0 +1,97 @@
+package br.com.rws.lojavirtual.loja_virtual_rws.NotaItemProduto;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import br.com.rws.lojavirtual.loja_virtual_rws.NotaFiscalCompra.NotaFiscalCompraModel;
+import br.com.rws.lojavirtual.loja_virtual_rws.Produto.ProdutoModel;
+
+@Entity
+@Table(name = "tb_nota_item_produto")
+@SequenceGenerator(name = "seq_nota_item_produto", sequenceName = "seq_nota_item_produto", allocationSize = 1, initialValue = 1)
+public class NotaItemProdutoModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_item_produto")
+    @Column(name = "nip_id")
+    private Long id;
+
+    @Column(name = "nip_quantidade", nullable = false)
+    private Double quantidade;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+    private ProdutoModel produto;
+
+    @ManyToOne
+    @JoinColumn(name = "nota_fiscal_compra_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_fiscal_compra_fk"))
+    private NotaFiscalCompraModel notaFiscalCompra;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public ProdutoModel getProduto() {
+        return produto;
+    }
+
+    public void setProduto(ProdutoModel produto) {
+        this.produto = produto;
+    }
+
+    public NotaFiscalCompraModel getNotaFiscalCompra() {
+        return notaFiscalCompra;
+    }
+
+    public void setNotaFiscalCompra(NotaFiscalCompraModel notaFiscalCompra) {
+        this.notaFiscalCompra = notaFiscalCompra;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        NotaItemProdutoModel other = (NotaItemProdutoModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
