faed0d71ac862e7ef0faa0e52337715e784819e9
RobWeenden <weendenandrade@gmail.com>
2022-01-28 12:31:36 -0300

    add class status_rastreio

---
+++ src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/StatusRastreio/StatusRastreioModel.java
@@ -0,0 +1,118 @@
+package br.com.rws.lojavirtual.loja_virtual_rws.StatusRastreio;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import br.com.rws.lojavirtual.loja_virtual_rws.VendaCompraLoja.VendaCompraLojaModel;
+
+@Entity
+@Table(name = "tb_status_rastreio")
+@SequenceGenerator(name = "seq_status_rastreio", sequenceName = "seq_status_rastreio", allocationSize = 1, initialValue = 1)
+public class StatusRastreioModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_status_rastreio")
+    @Column(name = "str_id")
+    private Long id;
+
+    @Column(name = "str_centro_distrib")
+    private String centroDistribuicao;
+
+    @Column(name = "str_cidade")
+    private String cidade;
+
+    @Column(name = "str_estado")
+    private String estado;
+
+    @Column(name = "str_status")
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "venda_compra_loja_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "venda_compra_loja_fk"))
+    private VendaCompraLojaModel vendaCompraLoja;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCentroDistribuicao() {
+        return centroDistribuicao;
+    }
+
+    public void setCentroDistribuicao(String centroDistribuicao) {
+        this.centroDistribuicao = centroDistribuicao;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        StatusRastreioModel other = (StatusRastreioModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public VendaCompraLojaModel getVendaCompraLoja() {
+        return vendaCompraLoja;
+    }
+
+    public void setVendaCompraLoja(VendaCompraLojaModel vendaCompraLoja) {
+        this.vendaCompraLoja = vendaCompraLoja;
+    }
+
+}
