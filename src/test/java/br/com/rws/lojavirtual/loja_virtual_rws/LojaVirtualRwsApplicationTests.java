6c6e3ddff65f3a4884bae895d35435a804e97597
RobWeenden <weendenandrade@gmail.com>
2022-02-01 21:38:30 -0300

    init test roleAcesso

--- src/test/java/br/com/rws/lojavirtual/loja_virtual_rws/LojaVirtualRwsApplicationTests.java
+++ src/test/java/br/com/rws/lojavirtual/loja_virtual_rws/LojaVirtualRwsApplicationTests.java
@@ -1,13 +1,33 @@
 package br.com.rws.lojavirtual.loja_virtual_rws;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import br.com.rws.lojavirtual.loja_virtual_rws.RoleAcesso.RoleAcessoController;
+import br.com.rws.lojavirtual.loja_virtual_rws.RoleAcesso.RoleAcessoModel;
+import br.com.rws.lojavirtual.loja_virtual_rws.RoleAcesso.RoleAcessoRepository;
+import br.com.rws.lojavirtual.loja_virtual_rws.RoleAcesso.RoleAcessoService;
+
+@SpringBootTest(classes = LojaVirtualRwsApplication.class)
 class LojaVirtualRwsApplicationTests {
 
+	@Autowired
+	private RoleAcessoService roleService;
+
+	@Autowired
+	private RoleAcessoRepository roleRepository;
+
+	@Autowired
+	private RoleAcessoController roleController;
+
 	@Test
-	void contextLoads() {
+	void testCadastraAcesso() {
+		RoleAcessoModel role = new RoleAcessoModel();
+
+		role.setDescricao("ROLE_GESTOR");
+
+		roleController.salvarRole(role);
 	}
 
 }
