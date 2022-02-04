641d0dd1539c090b971bb03440f8e5f4a3b87391
RobWeenden <weendenandrade@gmail.com>
2022-02-03 22:56:43 -0300

    query para teste

--- src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/RoleAcesso/RoleAcessoRepository.java
+++ src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/RoleAcesso/RoleAcessoRepository.java
@@ -1,11 +1,16 @@
 package br.com.rws.lojavirtual.loja_virtual_rws.RoleAcesso;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
-public interface RoleAcessoRepository extends JpaRepository<RoleAcessoModel, Long>{
-    
+public interface RoleAcessoRepository extends JpaRepository<RoleAcessoModel, Long> {
+
+    @Query("select a from RoleAcessoModel a where upper(trim(a.descricao)) like %?1%")
+    List<RoleAcessoModel> buscarRoleDescricao(String desc);
 }
