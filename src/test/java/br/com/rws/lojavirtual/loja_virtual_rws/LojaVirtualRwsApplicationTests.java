90d53dc6e104541b6ab2f75451828600df7f85ce
RobWeenden <weendenandrade@gmail.com>
2022-02-03 22:55:12 -0300

    implementacao Teste Unitario

--- src/test/java/br/com/rws/lojavirtual/loja_virtual_rws/LojaVirtualRwsApplicationTests.java
+++ src/test/java/br/com/rws/lojavirtual/loja_virtual_rws/LojaVirtualRwsApplicationTests.java
@@ -1,5 +1,7 @@
 package br.com.rws.lojavirtual.loja_virtual_rws;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +10,10 @@ import br.com.rws.lojavirtual.loja_virtual_rws.RoleAcesso.RoleAcessoController;
 import br.com.rws.lojavirtual.loja_virtual_rws.RoleAcesso.RoleAcessoModel;
 import br.com.rws.lojavirtual.loja_virtual_rws.RoleAcesso.RoleAcessoRepository;
 import br.com.rws.lojavirtual.loja_virtual_rws.RoleAcesso.RoleAcessoService;
+import junit.framework.TestCase;
 
 @SpringBootTest(classes = LojaVirtualRwsApplication.class)
-class LojaVirtualRwsApplicationTests {
+class LojaVirtualRwsApplicationTests extends TestCase {
 
 	@Autowired
 	private RoleAcessoService roleService;
@@ -25,9 +28,33 @@ class LojaVirtualRwsApplicationTests {
 	void testCadastraAcesso() {
 		RoleAcessoModel role = new RoleAcessoModel();
 
-		role.setDescricao("ROLE_GESTOR");
+		role.setDescricao("ROLE_TECNICO");
+		assertEquals(true, role.getId() == null);
+		role = roleController.salvarRole(role).getBody();
+		assertEquals(true, role.getId() > 0);
+		assertEquals("ROLE_TECNICO", role.getDescricao());
 
-		roleController.salvarRole(role);
+		RoleAcessoModel role2 = new RoleAcessoModel();
+
+		role2 = roleRepository.findById(role.getId()).get();
+		assertEquals(role.getId(), role2.getId());
+
+		roleRepository.deleteById(role2.getId());
+
+		roleRepository.flush();
+
+		RoleAcessoModel role3 = new RoleAcessoModel();
+
+		role3 = roleRepository.findById(role2.getId()).orElse(null);
+		assertEquals(true, role3 == null);
+
+		role = new RoleAcessoModel();
+		role.setDescricao("ROLE_ALUNO");
+		role = roleController.salvarRole(role).getBody();
+
+		List<RoleAcessoModel> listRole = roleRepository.buscarRoleDescricao("ALUNO".trim().toUpperCase());
+		assertEquals(1, listRole.size());
+		roleRepository.deleteById(role.getId());
 	}
 
 }
