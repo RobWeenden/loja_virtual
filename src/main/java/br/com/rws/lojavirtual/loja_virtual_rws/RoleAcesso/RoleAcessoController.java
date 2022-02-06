27ed51eb883e163ed441e32fec7625b876d4096d
RobWeenden <weendenandrade@gmail.com>
2022-02-06 17:24:34 -0300

    add new endpoint delete, get

--- src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/RoleAcesso/RoleAcessoController.java
+++ src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/RoleAcesso/RoleAcessoController.java
@@ -1,9 +1,14 @@
 package br.com.rws.lojavirtual.loja_virtual_rws.RoleAcesso;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,5 +38,29 @@ public class RoleAcessoController {
         roleRepository.deleteById(roleModel.getId());
 
         return new ResponseEntity("Acesso Removido", HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @DeleteMapping(value = "**/deleteById/{id}")
+    public ResponseEntity deleteByIdRole(@PathVariable("id") Long id) {
+        roleRepository.deleteById(id);
+
+        return new ResponseEntity("Acesso Removido por Id", HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping(value = "**/searchRole/{id}")
+    public ResponseEntity<RoleAcessoModel> searchRole(@PathVariable("id") Long id) {
+        RoleAcessoModel role = roleRepository.findById(id).get();
+
+        return new ResponseEntity<RoleAcessoModel>(role, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping(value = "**/searchDescriptionRole/{desc}")
+    public ResponseEntity<List<RoleAcessoModel>> searchDescriptionRole(@PathVariable("desc") String desc) {
+        List<RoleAcessoModel> role = roleRepository.buscarRoleDescricao(desc);
+
+        return new ResponseEntity<List<RoleAcessoModel>>(role, HttpStatus.OK);
     }
 }
