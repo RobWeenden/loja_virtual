0d7fb9c3c85bf906ef0e6f9e70f1d0f044d583fd
RobWeenden <weendenandrade@gmail.com>
2022-02-14 22:04:55 -0300

    add method getAuthentication

--- src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/Security/JWTTokenAuthenticationService.java
+++ src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/Security/JWTTokenAuthenticationService.java
@@ -2,9 +2,11 @@ package br.com.rws.lojavirtual.loja_virtual_rws.Security;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -33,7 +35,29 @@ public class JWTTokenAuthenticationService {
         String token = TOKEN_PREFIX + " " + JWT;
 
         response.addHeader(HEADER_STRING, token);
+        liberacaoCors(response);
         response.getWriter().write("{\"Authorization\": \""+ token + "\"}");//USANDO PARA O POSTMAN - TESTE
         
+    }
+
+    public Authentication gAuthentication(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String token = request.getHeader(HEADER_STRING);
+        return null;
+    }
+
+    private void liberacaoCors(HttpServletResponse resp){
+        if(resp.getHeader("Access-Control-Allow-Origin") == null){
+            resp.addHeader("Access-Control-Allow-Origin", "*");
+        }
+        if(resp.getHeader("Access-Control-Allow-Headers") == null){
+            resp.addHeader("Access-Control-Allow-Headers", "*");
+        }
+        if(resp.getHeader("Access-Control-Request-Headers") == null){
+            resp.addHeader("Access-Control-Request-Headers", "*");
+        }
+        if(resp.getHeader("Access-Control-Allow-Methods") == null){
+            resp.addHeader("Access-Control-Allow-Methods", "*");
+        }
+
     }
 }
