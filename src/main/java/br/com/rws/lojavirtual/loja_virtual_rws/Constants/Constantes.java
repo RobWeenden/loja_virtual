6fd78ab041f6c2a622a39196a7beb5e515b17bb7
RobWeenden <weendenandrade@gmail.com>
2022-03-08 22:20:05 -0300

    add class constantes

---
+++ src/main/java/br/com/rws/lojavirtual/loja_virtual_rws/Constants/Constantes.java
@@ -0,0 +1,16 @@
+package br.com.rws.lojavirtual.loja_virtual_rws.Constants;
+
+public class Constantes {
+
+    public static final long EXPIRATION_TIME = 1728000000;
+    public static final String SECRET = "asldkfjksf93378*&7lalkdfj*@*#7DDHEUmaasdjf";
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String HEADER_STRING = "Authorization";
+
+    public Constantes() { 
+        //default    
+    }
+
+ 
+
+}
