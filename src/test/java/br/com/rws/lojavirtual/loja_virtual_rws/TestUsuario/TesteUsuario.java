ed8d0e8ba110e3b4a0ae6ecef525125cb060eeeb
RobWeenden <weendenandrade@gmail.com>
2022-03-22 22:34:27 -0300

    novos teste caracter Aleatorio e cadastro PessoaPJ

--- src/test/java/br/com/rws/lojavirtual/loja_virtual_rws/TestUsuario/TesteUsuario.java
+++ src/test/java/br/com/rws/lojavirtual/loja_virtual_rws/TestUsuario/TesteUsuario.java
@@ -6,36 +6,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
 import br.com.rws.lojavirtual.loja_virtual_rws.LojaVirtualRwsApplication;
-import br.com.rws.lojavirtual.loja_virtual_rws.Pessoa.PessoaFisicaModel;
-import br.com.rws.lojavirtual.loja_virtual_rws.Pessoa.PessoaJuridicModel;
+import br.com.rws.lojavirtual.loja_virtual_rws.Exceptions.CustomExceptions;
+import br.com.rws.lojavirtual.loja_virtual_rws.Pessoa.PessoaController;
+import br.com.rws.lojavirtual.loja_virtual_rws.Pessoa.PessoaJuridicaModel;
 import br.com.rws.lojavirtual.loja_virtual_rws.Pessoa.PessoaRepository;
 import br.com.rws.lojavirtual.loja_virtual_rws.Usuario.UsuarioService;
 import junit.framework.TestCase;
 
 @Profile("test")
 @SpringBootTest(classes = LojaVirtualRwsApplication.class)
-public class TesteUsuario extends TestCase{
+public class TesteUsuario extends TestCase {
 
     @Autowired
-    private UsuarioService usuarioService;
-
-    @Autowired
-    PessoaRepository pessoaRepository;
+    PessoaController pessoaController;
 
     @Test
-    public void testCadastroUsuario(){
+    public void testCadastroUsuario() throws CustomExceptions {
 
-        PessoaJuridicModel pessoaJuridica = new PessoaJuridicModel();
+        PessoaJuridicaModel pessoaJuridica = new PessoaJuridicaModel();
         pessoaJuridica.setCnpj("1231505225858558");
-        pessoaJuridica.setNome("_nome_aASLFDKJDKIA==AKDKEI");
-        pessoaJuridica.setEmail("nome@teste.com");
+        pessoaJuridica.setNome("_nome_*cym(Fu=WUgHVo6Un=_G");
+        pessoaJuridica.setEmail("nome@Fu=WUg.com");
         pessoaJuridica.setTelefone("8585548555");
         pessoaJuridica.setInscEstadual("6655855825");
         pessoaJuridica.setInscMunicipal("132158d5d58");
-        pessoaJuridica.setNomeFantasia("_fantsia_aakdlkf90rLKJDF9-93");
-        pessoaJuridica.setRazaoSocial("-razcao_laksdj9309FT093IASLF0");
+        pessoaJuridica.setNomeFantasia("_fantasia_V&7HE$3EaOPiPuy1&t2F");
+        pessoaJuridica.setRazaoSocial("_razao_social_V&7HE$3EaOPiPuy1&t2F");
 
-        pessoaRepository.save(pessoaJuridica);
+        pessoaController.savePessoaJuridica(pessoaJuridica);
         // PessoaFisicaModel pessoaFisica = new PessoaFisicaModel();
         // pessoaFisica.setCpf("123158558");
         // pessoaFisica.setNome("_nome_asdkfjkeadsfjjk");
@@ -45,5 +43,18 @@ public class TesteUsuario extends TestCase{
         // pessoaFisica.setEmpresa(pessoaFisica);
 
     }
-    
+
+    @Test
+    public void gerarCaracterAleatorio() {
+        String randomString = "AabcDeFfgGhHsSdDJjKkLlçÇQWwEeRrTtYyUuIiOoPpZzXxCcVvBbNnMn)=m!#@#$%¨&*(_+_=-0987654321[{]}";
+        StringBuilder sb = new StringBuilder(20);
+        for (int i = 0; i < 20; i++) {
+            int index = (int) (randomString.length() * Math.random());
+            sb.append(randomString.charAt(index));
+        }
+        System.out.println("_nome_"+sb.toString());
+        System.out.println("_fantasia_"+sb.toString());
+        System.out.println("_razao_social_"+sb.toString());
+    }
+
 }
